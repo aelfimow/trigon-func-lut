b791087e1f35dbc32dd923f0c659c5f5cfa64667
Alexander Elfimow <my.free.mail@gmx.de>
2023-02-13 19:35:27 +0100

    Added test loop for sin

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,57 @@
 #include <iostream>
 #include <sstream>
 #include <stdexcept>
-#include <map>
+#include <vector>
+#include <cmath>
+#include <numeric>
 #include "sin_lut_fp32.h"
 #include "cos_lut_fp32.h"
 
-int main(int argc, char *argv[])
+using fp32 = float;
+static_assert(sizeof(fp32) == 4U);
+
+using fp64 = double;
+static_assert(sizeof(fp64) == 8U);
+
+using fp80 = long double;
+static_assert(sizeof(fp80) >= 10U);
+
+using fp128 = __float128;
+static_assert(sizeof(fp128) == 16U);
+
+int main(int, char **)
 try
 {
-    if (argc != 2)
+    fp32 const pi = 3.1415926535897932384626433832795f;
+
+    std::vector<unsigned int> degrees(361U);    // from 0 to 360
+    std::iota(degrees.begin(), degrees.end(), 0U);
+
+    std::vector<fp32> radian_fp32;
+    radian_fp32.reserve(degrees.size());
+
+    for (auto const degree: degrees)
     {
-        throw std::invalid_argument("Usage: trigon sin|cos|tan|cot angle");
+        fp32 const value_fp32 = ((2.0f * pi * static_cast<fp32>(degree)) / 360.0f);
+
+        radian_fp32.push_back(value_fp32);
     }
 
-    std::string const func { argv[1] };
-
-    size_t angle = 0U;
+    for (size_t i = 0U; i < degrees.size(); ++i)
     {
-        std::stringstream ss { argv[2] };
-        ss >> angle;
-    }
+        fp32 const a = sinf(radian_fp32[i]);
 
-    std::cout << "Function: " << func << " " << angle << std::endl;
+        fp32 const b = sin_lut_fp32::lut[degrees[i]]();
+
+        fp32 const diff = fabsf(a - b);
+
+        fp32 const eps = 1E-9f;
+
+        if (diff >= eps)
+        {
+            std::cout << "sin_lut_fp32, " << degrees[i] << ": " << diff << std::endl;
+        }
+    }
 
     return EXIT_SUCCESS;
 }
