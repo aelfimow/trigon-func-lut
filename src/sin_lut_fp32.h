158747157880a315ef61b4327cba6203f64c6d4f
Alexander Elfimow <my.free.mail@gmx.de>
2023-02-13 19:02:21 +0100

    Basic implementation

--- src/sin_lut_fp32.h
+++ src/sin_lut_fp32.h
@@ -4,6 +4,12 @@
 
 namespace sin_lut_fp32
 {
+    using fp32 = float;
+    using lut_func_t = fp32 (*)();
+
+    constexpr unsigned int MAX_LUT_ELEM = 360U;
+
+    extern lut_func_t const lut[MAX_LUT_ELEM];
 }
 
 #endif
