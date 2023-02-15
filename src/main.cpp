519b4ceca447da3a51404dfcb760e43d3bc1469d
Alexander Elfimow <my.free.mail@gmx.de>
2023-02-15 19:22:56 +0100

    Moved constants

--- src/main.cpp
+++ src/main.cpp
@@ -19,11 +19,15 @@ static_assert(sizeof(fp80) >= 10U);
 using fp128 = __float128;
 static_assert(sizeof(fp128) == 16U);
 
+namespace fp32data
+{
+    fp32 const pi = 3.1415926535897932384626433832795f;
+    fp32 const macheps = 5.96046E-08f;
+}
+
 int main(int, char **)
 try
 {
-    fp32 const pi = 3.1415926535897932384626433832795f;
-
     std::vector<unsigned int> degrees(361U);    // from 0 to 360
     std::iota(degrees.begin(), degrees.end(), 0U);
 
@@ -32,7 +36,7 @@ try
 
     for (auto const degree: degrees)
     {
-        fp32 const value_fp32 = ((2.0f * pi * static_cast<fp32>(degree)) / 360.0f);
+        fp32 const value_fp32 = ((2.0f * fp32data::pi * static_cast<fp32>(degree)) / 360.0f);
 
         radian_fp32.push_back(value_fp32);
     }
@@ -45,9 +49,7 @@ try
 
         fp32 const diff = fabsf(a - b);
 
-        fp32 const eps = 1E-9f;
-
-        if (diff >= eps)
+        if (diff > fp32data::macheps)
         {
             std::cout << "sin_lut_fp32, " << degrees[i] << ": " << diff << std::endl;
         }
