135e488cde686eea8d1eb7c6eaacf4dec78b8b87
Alexander Elfimow <my.free.mail@gmx.de>
2023-02-15 19:31:41 +0100

    Function to test cos_lut_fp32

--- src/main.cpp
+++ src/main.cpp
@@ -56,12 +56,43 @@ namespace
             }
         }
     }
+
+    void check_cos_lut_fp32()
+    {
+        std::vector<unsigned int> degrees(361U);    // from 0 to 360
+        std::iota(degrees.begin(), degrees.end(), 0U);
+
+        std::vector<fp32> radian_fp32;
+        radian_fp32.reserve(degrees.size());
+
+        for (auto const degree: degrees)
+        {
+            fp32 const value_fp32 = ((2.0f * fp32data::pi * static_cast<fp32>(degree)) / 360.0f);
+
+            radian_fp32.push_back(value_fp32);
+        }
+
+        for (size_t i = 0U; i < degrees.size(); ++i)
+        {
+            fp32 const a = cosf(radian_fp32[i]);
+
+            fp32 const b = cos_lut_fp32::lut[degrees[i]]();
+
+            fp32 const diff = fabsf(a - b);
+
+            if (diff > fp32data::macheps)
+            {
+                std::cout << "cos_lut_fp32, " << degrees[i] << ": " << diff << std::endl;
+            }
+        }
+    }
 }
 
 int main(int, char **)
 try
 {
     check_sin_lut_fp32();
+    check_cos_lut_fp32();
 
     return EXIT_SUCCESS;
 }
