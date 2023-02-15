f72fb8baa9e551ef2b9399a1885869010c4e2942
Alexander Elfimow <my.free.mail@gmx.de>
2023-02-15 20:02:31 +0100

    Added function

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include <numeric>
 #include "sin_lut_fp32.h"
 #include "cos_lut_fp32.h"
+#include "tan_lut_fp32.h"
 
 using fp32 = float;
 static_assert(sizeof(fp32) == 4U);
@@ -86,6 +87,36 @@ namespace
             }
         }
     }
+
+    void check_tan_lut_fp32()
+    {
+        std::vector<unsigned int> degrees(tan_lut_fp32::MAX_LUT_ELEM);
+        std::iota(degrees.begin(), degrees.end(), 0U);
+
+        std::vector<fp32> radian_fp32;
+        radian_fp32.reserve(degrees.size());
+
+        for (auto const degree: degrees)
+        {
+            fp32 const value_fp32 = ((2.0f * fp32data::pi * static_cast<fp32>(degree)) / 360.0f);
+
+            radian_fp32.push_back(value_fp32);
+        }
+
+        for (size_t i = 0U; i < degrees.size(); ++i)
+        {
+            fp32 const a = tanf(radian_fp32[i]);
+
+            fp32 const b = tan_lut_fp32::lut[degrees[i]]();
+
+            fp32 const diff = fabsf(a - b);
+
+            if (diff > fp32data::macheps)
+            {
+                std::cout << "tan_lut_fp32, " << degrees[i] << ": " << diff << std::endl;
+            }
+        }
+    }
 }
 
 int main(int, char **)
@@ -93,6 +124,7 @@ try
 {
     check_sin_lut_fp32();
     check_cos_lut_fp32();
+    check_tan_lut_fp32();
 
     return EXIT_SUCCESS;
 }
