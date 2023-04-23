a86f47e1220279de36aa08e63c6cdc491d9b19de
Alexander Elfimow <my.free.mail@gmx.de>
2023-04-23 19:34:19 +0200

    tan: added file

---
+++ src/tan_lut_fp64.h
@@ -0,0 +1,17 @@
+#ifdef TAN_LUT_FP64_H
+#error Already included
+#else
+
+#define TAN_LUT_FP64_H  1
+
+namespace tan_lut_fp64
+{
+    using fp64 = double;
+    using lut_func_t = fp64 (*)();
+
+    constexpr unsigned int MAX_LUT_ELEM = 361U;
+
+    extern lut_func_t const lut[MAX_LUT_ELEM];
+}
+
+#endif
