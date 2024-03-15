dce4795c8ae3c1b13d574ecf87408598dd1c9626
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-15 20:41:57 +0100

    main: check cot_fp64

--- src/main.cpp
+++ src/main.cpp
@@ -7,11 +7,12 @@
 #include <numbers>
 #include "types.h"
 #include "sin_lut_fp32.h"
-#include "sin_lut_fp64.h"
 #include "cos_lut_fp32.h"
-#include "cos_lut_fp64.h"
 #include "tan_lut_fp32.h"
 #include "cot_lut_fp32.h"
+#include "sin_lut_fp64.h"
+#include "cos_lut_fp64.h"
+#include "cot_lut_fp64.h"
 
 namespace fp32data
 {
@@ -211,6 +212,41 @@ namespace fp64data
             }
         }
     }
+
+    fp64 cot(fp64 x)
+    {
+        return (cos(x) / sin(x));
+    }
+
+    void check_cot_lut()
+    {
+        std::vector<unsigned int> degrees(cot_lut_fp64::MAX_LUT_ELEM);
+        std::iota(degrees.begin(), degrees.end(), 0U);
+
+        std::vector<fp64> radian;
+        radian.reserve(degrees.size());
+
+        for (auto const degree: degrees)
+        {
+            fp64 const tmp_value = ((2.0 * std::numbers::pi_v<fp64> * static_cast<fp64>(degree)) / 360.0);
+
+            radian.push_back(tmp_value);
+        }
+
+        for (size_t i = 0U; i < degrees.size(); ++i)
+        {
+            fp64 const a = cot(radian[i]);
+
+            fp64 const b = cot_lut_fp64::lut[degrees[i]]();
+
+            fp64 const diff = fabs(a - b);
+
+            if (diff > fp64data::macheps)
+            {
+                std::cout << "cot_lut_fp64, " << degrees[i] << ": " << diff << std::endl;
+            }
+        }
+    }
 }
 
 int main(int, char **)
@@ -223,6 +259,7 @@ try
 
     fp64data::check_sin_lut();
     fp64data::check_cos_lut();
+    fp64data::check_cot_lut();
 
     return EXIT_SUCCESS;
 }
