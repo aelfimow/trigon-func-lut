0f2bdb6deff6fe7d1142fdae13c81d9b3d6a99bc
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-19 20:14:30 +0100

    check_fp32: added file

---
+++ src/check_fp32.h
@@ -0,0 +1,15 @@
+#ifdef CHECK_FP32_H
+#error Already included
+#else
+
+#define CHECK_FP32_H  1
+
+namespace check_fp32
+{
+    extern void check_sin();
+    extern void check_cos();
+    extern void check_tan();
+    extern void check_cot();
+}
+
+#endif
