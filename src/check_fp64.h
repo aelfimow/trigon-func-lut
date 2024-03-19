24d98a24c8773a7f21eff3b7264f89ca83d4738e
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-19 20:24:45 +0100

    check_fp64: added file

---
+++ src/check_fp64.h
@@ -0,0 +1,15 @@
+#ifdef CHECK_FP64_H
+#error Already included
+#else
+
+#define CHECK_FP64_H  1
+
+namespace check_fp64
+{
+    extern void check_sin();
+    extern void check_cos();
+    extern void check_tan();
+    extern void check_cot();
+}
+
+#endif
