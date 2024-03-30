79fcfcfbf628c492134cb5c322aca7e8e78dd204
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-30 21:33:29 +0100

    check_fp80: added function

--- src/check_fp80.cpp
+++ src/check_fp80.cpp
@@ -4,6 +4,7 @@
 #include "sin_lut_fp80.h"
 #include "cos_lut_fp80.h"
 #include "tan_lut_fp80.h"
+#include "cot_lut_fp80.h"
 #include "degree_to_rad.h"
 
 #include <iostream>
@@ -103,6 +104,41 @@ void check_fp80::check_tan()
         if (diff > macheps)
         {
             std::cout << "tan_lut_fp80, " << degrees[i] << ": " << diff << std::endl;
+        }
+    }
+}
+
+void check_fp80::check_cot()
+{
+    std::vector<unsigned int> degrees(cot_lut_fp80::MAX_LUT_ELEM);
+    std::iota(degrees.begin(), degrees.end(), 0U);
+
+    std::vector<fp80> radian;
+    radian.reserve(degrees.size());
+
+    for (auto const degree: degrees)
+    {
+        fp80 const tmp_value = degree_to_rad::to_rad(degree);
+
+        radian.push_back(tmp_value);
+    }
+
+    auto cot = [](fp80 x) -> fp80
+    {
+        return (cosl(x) / sinl(x));
+    };
+
+    for (size_t i = 0U; i < degrees.size(); ++i)
+    {
+        fp80 const a = cot(radian[i]);
+
+        fp80 const b = cot_lut_fp80::lut[degrees[i]]();
+
+        fp80 const diff = fabsl(a - b);
+
+        if (diff > macheps)
+        {
+            std::cout << "cot_lut_fp80, " << degrees[i] << ": " << diff << std::endl;
         }
     }
 }
