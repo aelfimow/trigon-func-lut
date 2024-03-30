7e36903e78e0dbe88d015231f254ab774fdedf46
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-30 21:07:59 +0100

    check_fp80: added function

--- src/check_fp80.cpp
+++ src/check_fp80.cpp
@@ -2,6 +2,7 @@
 
 #include "types.h"
 #include "sin_lut_fp80.h"
+#include "cos_lut_fp80.h"
 
 #include <iostream>
 #include <vector>
@@ -40,6 +41,36 @@ void check_fp80::check_sin()
         if (diff > macheps)
         {
             std::cout << "sin_lut_fp80, " << degrees[i] << ": " << diff << std::endl;
+        }
+    }
+}
+
+void check_fp80::check_cos()
+{
+    std::vector<unsigned int> degrees(cos_lut_fp80::MAX_LUT_ELEM);
+    std::iota(degrees.begin(), degrees.end(), 0U);
+
+    std::vector<fp80> radian_fp80;
+    radian_fp80.reserve(degrees.size());
+
+    for (auto const degree: degrees)
+    {
+        fp80 const value_fp80 = ((2.0 * std::numbers::pi_v<fp80> * static_cast<fp80>(degree)) / 360.0);
+
+        radian_fp80.push_back(value_fp80);
+    }
+
+    for (size_t i = 0U; i < degrees.size(); ++i)
+    {
+        fp80 const a = cosl(radian_fp80[i]);
+
+        fp80 const b = cos_lut_fp80::lut[degrees[i]]();
+
+        fp80 const diff = fabsl(a - b);
+
+        if (diff > macheps)
+        {
+            std::cout << "cos_lut_fp80, " << degrees[i] << ": " << diff << std::endl;
         }
     }
 }
