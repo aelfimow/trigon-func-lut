f4a46196ae7c18d9829a34e415e3da0943958ab8
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-30 20:45:17 +0100

    degree_to_rad: added file

---
+++ src/degree_to_rad.cpp
@@ -0,0 +1,26 @@
+#include "types.h"
+#include "degree_to_rad.h"
+
+#include <numbers>
+
+
+fp32 degree_to_rad::to_rad_f(unsigned int value)
+{
+    fp32 const grad = static_cast<fp32>(value);
+
+    return ((2.0f * std::numbers::pi_v<fp32> * grad) / 360.0f);
+}
+
+fp64 degree_to_rad::to_rad(unsigned int value)
+{
+    fp64 const grad = static_cast<fp64>(value);
+
+    return ((2.0 * std::numbers::pi_v<fp64> * grad) / 360.0);
+}
+
+fp80 degree_to_rad::to_rad_l(unsigned int value)
+{
+    fp80 const grad = static_cast<fp80>(value);
+
+    return ((2.0f * std::numbers::pi_v<fp80> * grad) / 360.0f);
+}
