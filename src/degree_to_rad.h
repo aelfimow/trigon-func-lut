ab35d6cb73d226d3d200fd0a60dcf13d2e503a8e
Alexander Elfimow <my.free.mail@gmx.de>
2024-03-30 20:45:03 +0100

    degree_to_rad: added file

---
+++ src/degree_to_rad.h
@@ -0,0 +1,14 @@
+#ifdef DEGREE_TO_RAD_H
+#error Already included
+#else
+
+#define DEGREE_TO_RAD_H  1
+
+namespace degree_to_rad
+{
+    extern fp32 to_rad_f(unsigned int value);
+    extern fp64 to_rad(unsigned int value);
+    extern fp80 to_rad_l(unsigned int value);
+}
+
+#endif
