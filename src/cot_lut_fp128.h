90d41c6ecd0f64365c0a92b5c798e84ef2838afe
Alexander Elfimow <my.free.mail@gmx.de>
2024-04-01 21:22:33 +0200

    cot_lut_fp128: added file

---
+++ src/cot_lut_fp128.h
@@ -0,0 +1,16 @@
+#ifdef COT_LUT_FP128_H
+#error Already included
+#else
+
+#define COT_LUT_FP128_H     1
+
+namespace cot_lut_fp128
+{
+    using lut_func_t = fp128 (*)();
+
+    constexpr unsigned int MAX_LUT_ELEM = 361U;
+
+    extern lut_func_t const lut[MAX_LUT_ELEM];
+}
+
+#endif
