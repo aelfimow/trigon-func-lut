950a61ff289df52a70bdd5cbadea02b5292aacb7
Alexander Elfimow <my.free.mail@gmx.de>
2024-04-01 20:25:22 +0200

    degree_to_rad: added function

--- src/degree_to_rad.h
+++ src/degree_to_rad.h
@@ -9,6 +9,7 @@ namespace degree_to_rad
     extern fp32 to_rad_f(unsigned int value);
     extern fp64 to_rad(unsigned int value);
     extern fp80 to_rad_l(unsigned int value);
+    extern fp128 to_rad_q(unsigned int value);
 }
 
 #endif
