ad112f4a58e7710b9fc34aae5a4a9b1c9d4dadc0
Alexander Elfimow <my.free.mail@gmx.de>
2024-04-01 21:11:10 +0200

    check_fp128: added function

--- src/check_fp128.cpp
+++ src/check_fp128.cpp
@@ -3,6 +3,7 @@
 #include "types.h"
 #include "sin_lut_fp128.h"
 #include "cos_lut_fp128.h"
+#include "tan_lut_fp128.h"
 #include "degree_to_rad.h"
 
 #include <iostream>
@@ -83,6 +84,36 @@ void check_fp128::check_cos()
         if (diff > FLT128_EPSILON)
         {
             std::cout << "cos_lut_fp128, " << degrees[i] << ": " << diff << std::endl;
+        }
+    }
+}
+
+void check_fp128::check_tan()
+{
+    std::vector<unsigned int> degrees(tan_lut_fp128::MAX_LUT_ELEM);
+    std::iota(degrees.begin(), degrees.end(), 0U);
+
+    std::vector<fp128> radians;
+    radians.reserve(degrees.size());
+
+    for (auto const degree: degrees)
+    {
+        fp128 const rad = degree_to_rad::to_rad_q(degree);
+
+        radians.push_back(rad);
+    }
+
+    for (size_t i = 0U; i < degrees.size(); ++i)
+    {
+        fp128 const a = ::tanq(radians[i]);
+
+        fp128 const b = tan_lut_fp128::lut[degrees[i]]();
+
+        fp128 const diff = ::fabsq(a - b);
+
+        if (diff > FLT128_EPSILON)
+        {
+            std::cout << "tan_lut_fp128, " << degrees[i] << ": " << diff << std::endl;
         }
     }
 }
