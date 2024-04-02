f07107043a4d6db3aec49e30ae1ecda4c62b85ae
Alexander Elfimow <my.free.mail@gmx.de>
2024-04-02 20:14:21 +0200

    check_fp32: renamed variable

--- src/check_fp32.cpp
+++ src/check_fp32.cpp
@@ -58,9 +58,9 @@ void check_fp32::check_cos()
 
     for (auto const degree: degrees)
     {
-        fp32 const value_fp32 = degree_to_rad::to_rad_f(degree);
+        fp32 const rad = degree_to_rad::to_rad_f(degree);
 
-        radian_fp32.push_back(value_fp32);
+        radian_fp32.push_back(rad);
     }
 
     for (size_t i = 0U; i < degrees.size(); ++i)
