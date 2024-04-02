f905649c43f27ea58c59032a803c1b2bca5a963c
Alexander Elfimow <my.free.mail@gmx.de>
2024-04-02 20:13:58 +0200

    check_fp32: renamed container

--- src/check_fp32.cpp
+++ src/check_fp32.cpp
@@ -23,19 +23,19 @@ void check_fp32::check_sin()
     std::vector<unsigned int> degrees(sin_lut_fp32::MAX_LUT_ELEM);
     std::iota(degrees.begin(), degrees.end(), 0U);
 
-    std::vector<fp32> radian_fp32;
-    radian_fp32.reserve(degrees.size());
+    std::vector<fp32> radians;
+    radians.reserve(degrees.size());
 
     for (auto const degree: degrees)
     {
         fp32 const value_fp32 = degree_to_rad::to_rad_f(degree);
 
-        radian_fp32.push_back(value_fp32);
+        radians.push_back(value_fp32);
     }
 
     for (size_t i = 0U; i < degrees.size(); ++i)
     {
-        fp32 const a = sinf(radian_fp32[i]);
+        fp32 const a = sinf(radians[i]);
 
         fp32 const b = sin_lut_fp32::lut[degrees[i]]();
 
