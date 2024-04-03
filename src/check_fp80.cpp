6aa7e93263ab22e283b3a3d019775a3ecb5c0c87
Alexander Elfimow <my.free.mail@gmx.de>
2024-04-03 20:21:54 +0200

    check_fp80: renamed variable

--- src/check_fp80.cpp
+++ src/check_fp80.cpp
@@ -28,9 +28,9 @@ void check_fp80::check_sin()
 
     for (auto const degree: degrees)
     {
-        fp80 const value_fp80 = degree_to_rad::to_rad_l(degree);
+        fp80 const rad = degree_to_rad::to_rad_l(degree);
 
-        radians.push_back(value_fp80);
+        radians.push_back(rad);
     }
 
     for (size_t i = 0U; i < degrees.size(); ++i)
